fd96725ab52413944a4f0b0001bf603fc687cde6
Mohammad Ayaz <shaikhayaz123321@gmail.com>
2024-09-10 22:54:44 +0530

    Naive Solution insert at beginning

--- LinkedList/CircularLinkedList.java
+++ LinkedList/CircularLinkedList.java
@@ -6,11 +6,13 @@ public class CircularLinkedList {
         
         
         Node head = new Node(10);
+
         head.next = new Node(1890);
         head.next.next = new Node(90);
         head.next.next.next = new Node(189);
         head.next.next.next.next=head;
         
+        head = insertAtBeginnig(head, new Node(900));
         traverseLinked(head);
     }
 
@@ -23,5 +25,23 @@ public class CircularLinkedList {
             System.out.println(curr.data);
             curr= curr.next;
         }
+    }
+
+    public static  Node insertAtBeginnig( Node head , Node nodeToBeInserted){
+
+        if(head == null){
+            nodeToBeInserted.next = nodeToBeInserted;
+            return nodeToBeInserted;
+        }
+        Node curr = head;
+        while (curr.next!=head) {
+            curr= curr.next;
+        }
+
+        nodeToBeInserted.next=head;
+        curr.next=nodeToBeInserted;
+
+        return nodeToBeInserted;
+
     }
 }
