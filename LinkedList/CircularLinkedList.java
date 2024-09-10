2684e6b4908051550b5e3757f4f6aec61a419867
Mohammad Ayaz <shaikhayaz123321@gmail.com>
2024-09-10 22:28:38 +0530

    Circular Linked List and Travesal

--- LinkedList/CircularLinkedList.java
+++ LinkedList/CircularLinkedList.java
@@ -15,9 +15,11 @@ public class CircularLinkedList {
     }
 
     public static void traverseLinked( Node head){
-        Node curr = head;
-        
-        while (curr.next != head) {
+
+        if(head == null) return;
+        Node curr = head.next;
+        System.out.println(head.data);
+        while (curr!= head) {
             System.out.println(curr.data);
             curr= curr.next;
         }
